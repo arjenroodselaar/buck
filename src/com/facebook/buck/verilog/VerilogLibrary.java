fa45c46fb4399592e4add59a5efd4f8367fe585c
Arjen Roodselaar <arjen@fb.com>
2017-09-18 10:13:45 -0700

    Make Verilog design library parent directory

--- src/com/facebook/buck/verilog/VerilogLibrary.java
+++ src/com/facebook/buck/verilog/VerilogLibrary.java
@@ -10,6 +10,7 @@ import com.facebook.buck.rules.BuildableContext;
 import com.facebook.buck.rules.BuildRuleParams;
 import com.facebook.buck.rules.ExplicitBuildTargetSourcePath;
 import com.facebook.buck.rules.SourcePath;
+import com.facebook.buck.step.fs.MkdirStep;
 import com.facebook.buck.step.fs.RmStep;
 import com.facebook.buck.step.Step;
 import com.facebook.buck.verilog.VerilogLibraryCreateStep;
@@ -32,11 +33,20 @@ public class VerilogLibrary extends AbstractBuildRuleWithDeclaredAndExtraDeps {
   @Override
   public ImmutableList<Step> getBuildSteps(
       BuildContext context, BuildableContext buildableContext) {
-    BuildCellRelativePath cellRelativeOutput =
+    BuildCellRelativePath cellRelativeParent =
         BuildCellRelativePath.fromCellRelativePath(
-            context.getBuildCellRootPath(), getProjectFilesystem(), getOutput());
+            context.getBuildCellRootPath(),
+            getProjectFilesystem(),
+            getOutput().getParent());
+    BuildCellRelativePath cellRelativeOutput =
+            BuildCellRelativePath.fromCellRelativePath(
+                context.getBuildCellRootPath(), getProjectFilesystem(), getOutput());
 
     return ImmutableList.<Step>builder()
+        // Use of MakeCleanDirectoryStep is adviced, but the create step will
+        // fail if the output directory already exists. Therefor, just create
+        // the output parent directory and clean the design library directory.
+        .add(MkdirStep.of(cellRelativeParent))
         .add(RmStep.of(cellRelativeOutput))
         .add(new VerilogLibraryCreateStep(
             getProjectFilesystem().getRootPath(), // working directory
